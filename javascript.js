d91c8be585d9b80dcdba40ddb6c8b8707753cf45
Devon Ward <devoncward@gmail.com>
2019-05-07 19:19:20 -0600

    ping pong javascript working

--- javascript.js
+++ javascript.js
@@ -1,17 +1,39 @@
+window.onload = function() {
+  movePingPongBall();
+};
 function printConsole() {
   console.log("test worked");
 }
 function movePingPongBall() {
-  var element = document.getElementById("pingpongballelement");
-  var height = 0;
-  var length = 0;
+  console.log("ping pong ball should move now");
+  var element = document.getElementById("dot");
+  var height = 100;
+  var length = 200;
+  var counter = 0;
+  var moveUp = -1;
+  var moveAcross = 1;
   var id = setInterval(frame, 10);
   function frame() {
-    if(height == 350) {
+    if(counter == 10000) {
       clearInterval(id);
     } else {
-      height++;
-      length++;
+      if(height == 150 || height == 100) {
+        moveUp *= -1;
+      }
+      if(length == 1400 || length == 200) {
+        moveAcross *= -1;
+      }
+      if(moveUp == 1) {
+        height++;
+      } else {
+        height--;
+      }
+      if(moveAcross == 1) {
+        length--;
+      } else {
+        length++;
+      }
+      counter++;
       element.style.top = height + 'px';
       element.style.left = length + 'px';
     }
